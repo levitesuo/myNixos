a3d7f6626feb077b56f01d981d10b210584ba499
Leevi Suotula <leevi.suotula@gmail.com>
2026-02-27 11:19:56 +0200

    Added some stuff

--- home/hypr/inputs.nix
+++ home/hypr/inputs.nix
@@ -1,7 +1,14 @@
 { ... }:
 {
-    wayland.windowManager.hyprland.settings.input = {
-        kb_layout = "us";
-        kb_variant = "altgr-intl";
+    wayland.windowManager.hyprland.settings = {
+            input = {
+                kb_layout = "us";
+                kb_variant = "altgr-intl";
+            };
+            device = [{
+                name = "at-translated-set-2-keyboard";
+                kb_layout = "fi";
+                kb_variant = "";
+            }];
     };
-}
+}                
