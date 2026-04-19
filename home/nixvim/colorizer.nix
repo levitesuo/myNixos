30f4976380638eaa9b4ee9341addf95a00c9c6a6
leevisuo <leevi.suotula@gmail.com>
2026-04-19 12:28:33 +0300

    Added more contrast to colortheme

--- home/nixvim/colorizer.nix
+++ home/nixvim/colorizer.nix
@@ -3,30 +3,16 @@
   programs.nixvim.plugins.colorizer = {
     enable = true;
     settings = {
-      filetypes = {
-        "*" = {
-          mode = "background";
-        };
-        css = {
-          css = true;
-          css_fn = true;
-        };
-      };
+      filetypes = [ "*" ];
       user_default_options = {
         RGB = true;
         RRGGBB = true;
         names = true;
         RRGGBBAA = true;
-        AARRGGBB = false;
         rgb_fn = true;
         hsl_fn = true;
-        css = true;
-        css_fn = true;
         mode = "background";
         tailwind = true;
-        sass = {
-          enable = true;
-        };
         always_update = false;
       };
     };
